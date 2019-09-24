e14d2d8d2a25e1cee42a08296d16da4c2ab4ea1d
gyeomo <canino@naver.com>
2019-09-24 21:30:44 +0900

    connect success

--- NUSM/app/src/main/java/com/dev/kih/nusm/ApiClient.java
+++ NUSM/app/src/main/java/com/dev/kih/nusm/ApiClient.java
@@ -4,6 +4,9 @@ import android.util.Log;
 
 import com.google.gson.Gson;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.MediaType;
@@ -17,7 +20,7 @@ import java.lang.*;
 
 public class ApiClient {
     private String URL = "http://203.252.91.45";//"http://127.0.0.1"; //return: 203.252.91.45
-    private String PORT = ":3000/";
+    private String PORT = ":3001/";
     private String url_media = "event/";
 
     public boolean testGet(){
@@ -50,16 +53,19 @@ public class ApiClient {
 
             String url = URL+PORT+url_media+"token";
 
-            token = "{\"data\": "+ "\""+token +"\""+"}";
-
-            Gson gson = new Gson();
-            String json = gson.toJson(token);
-            Log.d("token: ", token);
-            Log.d("token to json: ",json);
+            JSONObject postdata = new JSONObject();
+            try {
+                postdata.put("data", token);
+            } catch(JSONException e){
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
 
             Request request = new Request.Builder()
                     .url(url)
-                    .post(RequestBody.create(MediaType.parse("application/json"), token))
+                    .post(RequestBody.create(MediaType.parse("application/json"), postdata.toString()))
+                    .header("Accept", "application/json")
+                    .header("Content-Type", "application/json")
                     .build();
 
             client.newCall(request).enqueue(sendTokenCallback);
